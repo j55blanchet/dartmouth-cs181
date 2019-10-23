6d48377332c9e2fe85be82407eadf90e0c6e0579
Julien Blanchet <j55blanchet@gmail.com>
2019-10-23 00:37:57 -0400

    starting pa3

--- pa-3/src/pa3/scripts/pa3.py
+++ pa-3/src/pa3/scripts/pa3.py
@@ -3,13 +3,38 @@ from __future__ import print_function, division
 
 import rospy
 from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose
 
 
-def path_finder(input, goal):
+def find_path(start, goal, grid_topic="/map"):
+    """Finds a path from the starting node to the goal node, based off an occupancy grid 
+    
+    Arguments:
+        start {Pose} -- The starting pose (must be in occupancy grid reference frame)
+        goal {Pose} -- The goal pose (must be in the occupancy grid reference frame)
+    
+    Keyword Arguments:
+        grid_topic {str} -- The topic where an OccupancyGrid is published (default: {"/map"})
+    
+    Returns:        
+        [type] -- [description]
+    """
+
+    # First, load the map
+    rospy.loginfo("Loading map")
+    grid = rospy.wait_for_message(grid_topic, OccupancyGrid, 5)
+
+        
+    print("Grid: " + grid)
+    poses = []
+    
+    return poses
+
+
+def publish_path(pose_seq):
     pass
 
 
-
-
-if __name__ == "__main__":
-    pass
+if __name__ == "__main__":  
+    pose_seq = find_path(None, None)
+    publish_path(pose_seq)
