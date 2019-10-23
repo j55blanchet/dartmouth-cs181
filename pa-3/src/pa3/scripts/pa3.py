06037fdcf56896f9d758c420c30848b9c2f9cf67
Julien Blanchet <j55blanchet@gmail.com>
2019-10-22 20:30:57 -0400

    Starting structure for pa3

---
+++ pa-3/src/pa3/scripts/pa3.py
@@ -0,0 +1,15 @@
+
+from __future__ import print_function, division
+
+import rospy
+from nav_msgs.msg import OccupancyGrid
+
+
+def path_finder(input, goal):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    pass
