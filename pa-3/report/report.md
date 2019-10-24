90a59f433c97ad5ed4a846a6f5d743f3544dab3f
Julien Blanchet <j55blanchet@gmail.com>
2019-10-24 17:47:47 -0400

    A* working!

---
+++ pa-3/report/report.md
@@ -0,0 +1,13 @@
+# Programming Assignment #3
+Julien Blanchet &nbsp;&bull;&nbsp; COSC 181  &nbsp;&bull;&nbsp;Fall 2019  
+
+<hr>
+Project Link: https://github.com/j55blanchet/dartmouth-cs181
+<hr>
+
+## Test Cases
+* Start: (0.1, 0.1), Goal: (9.5, 9.5) --> should fail because start cell is occupied
+* Start: (0.7, 0.7), Goal: (9.5, 9.5) --> should fail because goal cell is occupied
+
+## Allocation of Effort
+All coding, mathmatical calculations, and documentation on this project was done independently. I consulted various online programming resources while working on this assignment - citations are in the relevant comments. I also discussed high-level strategy and approaches for this assignment with peers from class and the robotics lab.
--- pa-3/src/pa3/scripts/pa3.py
+++ pa-3/src/pa3/scripts/pa3.py
@@ -2,13 +2,19 @@
 
 from __future__ import print_function, division
 
+import math
+import random
+from Queue import PriorityQueue
+
 import rospy
 from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import Pose, Point, Vector3
+from geometry_msgs.msg import Pose, PoseStamped, Point, Vector3
 from std_msgs.msg import Header, ColorRGBA
 from visualization_msgs.msg import Marker
 
 TOPIC_TIMEOUT = 20
+VISUALIZATION_TIME = 180 # 3 mins
+SQRT2 = math.sqrt(2)
 
 # Online Documentation Links
 #   
@@ -17,10 +23,11 @@ TOPIC_TIMEOUT = 20
 #   Point: http://docs.ros.org/melodic/api/geometry_msgs/html/msg/Point.html
 #   OccupancyGrid: http://docs.ros.org/kinetic/api/nav_msgs/html/msg/OccupancyGrid.html
 
-def find_path(start, goal):
-    """Finds a path from the starting node to the goal node, based off an occupancy grid 
-    
+def find_path(marker_pub, grid, start, goal):
+    """Finds a path from the starting node to the goal node, based off an occupancy grid
+
     Arguments:
+        grid {OccupancyGrid} -- The grid with obstacles to find a path around
         start {Point} -- The starting pose (must be in occupancy grid reference frame)
         goal {Point} -- The goal pose (must be in the occupancy grid reference frame)
     
@@ -28,15 +35,137 @@ def find_path(start, goal):
         grid_topic {str} -- The topic where an OccupancyGrid is published (default: {"/map"})
     
     Returns:        
-        [Pose] -- A path (sequence of poses) that the robot should take to get to the destination (empty if this is impossible)
+        [PoseStamped] -- A path (sequence of poses) that the robot should take to get to the destination (empty if this is impossible)
     """
+    (start_col, start_row) = get_cell_for_point(grid, start)
+    (goal_col, goal_row) = get_cell_for_point(grid, goal)
 
+    if start_col < 0 or start_col >= grid.info.width or start_row < 0 or start_row >= grid.info.height or \
+       goal_col  < 0 or goal_col  >= grid.info.width or goal_row  < 0 or goal_row  >= grid.info.height:       
+        rospy.logwarn("No path could be found. Starting point %s or ending point %s were out of bounds" % (format_point(start), format_point(goal)))
+        return []
+
+    visualize_cell(marker_pub, grid, start_col, start_row, ColorRGBA(0, 1.0, 1.0, 1.0))
+    visualize_cell(marker_pub, grid, goal_col, goal_row, ColorRGBA(0, 0.7, 0, 1.0))
+
+    if is_occupied(grid, start_col, start_row):
+        rospy.logwarn("No path could be found. Starting point %s is in an occupied cell" % format_point(start))
+        return []
+    
+    if is_occupied(grid, goal_col, goal_row):
+        rospy.logwarn("No path could be found. Goal point %s is in an occupied cell" % format_point(goal))
+        return []
+
+    if start_col is goal_col and start_row is goal_row:
+        rospy.loginfo("Start point %s and end point %s are in the same occupancy cell (%d, %d). Returning empty path" %(format_point(start), format_point(goal), start_col, start_row))
+        return []
+        
+    def calc_priority(col, row, cost):
+        """Calculates and returns the priority of the cell based of the existing path cost and remaining path heuristic"""
+        return cost + math.hypot(goal_col - col, goal_row - row)
+
+    def get_empty_neighbors(col, row):
+        """Returns the neighbor cells in the form of (col, row, delta_cost) to the current cell, using 8-way connectivity"""
+        neighbors = [
+            (col - 1, row - 1, SQRT2), (col, row - 1, 1), (col + 1, row - 1, SQRT2),
+            (col - 1, row, 1),                            (col + 1, row, 1),
+            (col - 1, row + 1, SQRT2), (col, row + 1, 1), (col + 1, row + 1, SQRT2)
+        ]
+        onmap_neighbors = [n for n in neighbors if n[0] >= 0 and n[0] < grid.info.width and n[1] >= 0 and n[1] < grid.info.height]
+        return [n for n in onmap_neighbors if not is_occupied(grid, col, row)]
+
+    # This function uses an A* algorithm. 
+    #   > The priority is calculated as cost(node) + heuristic(node).
+    #   > cost(node) = path length from start to node
+    #   > heuristic(node) = euclidian distance from node to goal
+
+    #
+    # Entries in the frontier are formatted as (priority, col, row, path_cost)
+    #   > using a priority queue ensures that we'll always be processing the node
+    #     with the lowest estimated total path cost (which is the priority)
+        
+    min_path_costs = {
+        (start_col, start_row): 0
+    }
+    back_pointers = {}
+
+    frontier = PriorityQueue()
+    frontier.put((calc_priority(start_col, start_row, 0), start_col, start_row, 0))
+
+    
+    while not frontier.empty():
+        (_, col, row, path_cost) = frontier.get()
+
+        # If there was a quicker way found to this node, ignore it
+        if path_cost is not min_path_costs[(col, row)]:
+            continue
+
+        neighbors = get_empty_neighbors(col, row)
+
+        for (ncol, nrow, dcost) in neighbors:
+            npath_cost = path_cost + dcost
+
+            # If this node has already been explored with a faster min_path_cost, ignore it
+            if (ncol, nrow) in min_path_costs and min_path_costs[(ncol, nrow)] <= npath_cost:
+                continue
+
+            min_path_costs[(ncol, nrow)] = npath_cost
+            npriority = calc_priority(ncol, nrow, npath_cost)
+            frontier.put((npriority, ncol, nrow, npath_cost))
+            back_pointers[(ncol, nrow)] = (col, row)
+
+            if ncol is goal_col and nrow is goal_row:
+                break
+
+        if (goal_col, goal_row) in back_pointers:
+            break
+
+    # Reconstruct the path (while loop won't execute if no path was found)
     poses = []
-    # TODO: Implement A* Algorithm
+    final_path_length = 0
+    cell = (goal_col, goal_row)
+    while cell in back_pointers:
+        point = get_point_for_cell(grid, cell[0], cell[1])
+        pose = PoseStamped(
+            header=Header(frame_id='map'),
+            pose=Pose(position=point)
+        )
+        # TODO: Determine orientation
+        poses.append(pose)
+        cell = back_pointers[cell]
+
+        npoint = get_point_for_cell(grid, cell[0], cell[1])
+        final_path_length += math.hypot(point.x - npoint.x, point.y - npoint.y)
+
+    if len(poses) is 0:
+        rospy.loginfo("No path was found from start %s to goal %s. Returning empty path" % (format_point(start), format_point(goal)))
+    else:
+        rospy.loginfo("Found path of length %0.2f found from start %s to goal %s! (%d cells)" % (final_path_length, format_point(start), format_point(goal), len(poses)))
     return poses
 
-def publish_path(pose_seq):
-    pass
+def format_point(point):
+    return "(%0.3f, %0.3f)" % (point.x, point.y)
+
+def publish_path(marker_pub, pose_seq):
+    
+    # Publish poses (for use by robot)
+    pose_pub = rospy.Publisher("/pose_sequence", PoseStamped, queue_size=1)
+    for pose in pose_seq:
+        pose_pub.publish(pose)
+    pose_pub.unregister()
+
+    # Add visualization to rviz
+    marker = Marker(
+            type=Marker.LINE_STRIP,
+            action=Marker.ADD,
+            id=0,
+            lifetime=rospy.Duration(VISUALIZATION_TIME), # forever
+            scale=Vector3(0.1, 0.1, 0.1),
+            header=Header(frame_id='map'),
+            points=[p.pose.position for p in pose_seq],
+            color=ColorRGBA(0.0, 1.0, 0.0, 1.0)
+        )
+    marker_pub.publish(marker)
 
 def get_cell_for_point(grid, p):
     """Gets the column and row that a point falls on
@@ -48,23 +177,32 @@ def get_cell_for_point(grid, p):
     Returns:
         (Int, Int) -- The (column, row) of the 
     """
-    col = int(grid.info.width * grid.info.resolution / p.x)
-    row = int(grid.info.width * grid.info.resolution / p.y)
+    col = int(p.x / grid.info.resolution)
+    row = int(p.y / grid.info.resolution)
     return (col, row)
 
-def get_cell_data(grid, col, row):
-    return grid.data[col * grid.info.height + row]
+def is_occupied(grid, col, row):
+    return grid.data[col + grid.info.width * row] > 50
 
 def get_point_for_cell(grid, col, row):
     x = grid.info.origin.position.x + grid.info.resolution * col
     y = grid.info.origin.position.y + grid.info.resolution * row
     return Point(x=x, y=y)
 
-
-
-def visualize_grid_occupancy(grid):
-    marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=20)
-
+def visualize_cell(marker_pub, grid, col, row, color):
+    marker = Marker(
+            type=Marker.SPHERE,
+            action=Marker.ADD,
+            id= (1 + col * grid.info.width + row),
+            lifetime=rospy.Duration(secs=VISUALIZATION_TIME),
+            scale=Vector3(0.3, 0.3, 0.3),
+            header=Header(frame_id='map'),
+            pose=Pose(position=get_point_for_cell(grid, col, row)),
+            color=color
+    )
+    marker_pub.publish(marker)
+    
+def visualize_grid_occupancy(marker_pub, grid):
     row = 0
     rate = rospy.Rate(4)
 
@@ -74,10 +212,9 @@ def visualize_grid_occupancy(grid):
         colors = []
 
         for col in range(grid.info.width):
-            data = get_cell_data(grid, col, row)
             point = get_point_for_cell(grid, col, row)
             points.append(point)
-            if data > 50:
+            if is_occupied(grid, col, row):
                 colors.append(ColorRGBA(1.0, 0.0, 0.0, 0.5))
             else:
                 colors.append(ColorRGBA(0.0, 1.0, 0.0, 0.5))
@@ -86,8 +223,8 @@ def visualize_grid_occupancy(grid):
             type=Marker.POINTS,
             action=Marker.ADD,
             id=0,
-            lifetime=rospy.Duration(1.5),
-            scale=Vector3(0.5, 0.5, 0.5),
+            lifetime=rospy.Duration(secs=0.25),
+            scale=Vector3(0.1, 0.1, 0.1),
             header=Header(frame_id='map'),
             points=points,
             colors=colors
@@ -97,9 +234,19 @@ def visualize_grid_occupancy(grid):
         rate.sleep()
 
 if __name__ == "__main__":  
-    rospy.init_node("pa3-path-finder")
+    rospy.init_node("path_finder")
     grid = rospy.wait_for_message("/map", OccupancyGrid, TOPIC_TIMEOUT)
-    visualize_grid_occupancy(grid)
 
-    # pose_seq = find_path(None, None)
-    # publish_path(pose_seq)
+    w = grid.info.resolution * grid.info.width
+    h = grid.info.resolution * grid.info.height
+
+    start = Point(random.random() * w, random.random() * h, 0)
+    end = Point(random.random() * w, random.random() * h, 0)
+
+    marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=20)
+
+    rospy.loginfo("Grid Info: " + str(grid.info))
+
+    pose_seq = find_path(marker_pub, grid, start, end)
+    publish_path(marker_pub, pose_seq)
+    # visualize_grid_occupancy(marker_pub, grid)
